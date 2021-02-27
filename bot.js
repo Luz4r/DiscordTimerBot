4fa381cd5af75428b2cb0ef74e57365a3df4b510
Luz4r <piorek.r@gmail.com>
2021-02-27 03:23:48 +0100

    Reset timers when day changed

--- bot.js
+++ bot.js
@@ -5,15 +5,24 @@ require('dotenv').config();
 var users = new Discord.Collection();
 var server;
 var voiceChannels = [];
+var today = new Date();
+
+function getStringDate(inDate){
+    let day = ("0" + inDate.getDate()).slice(-2);
+    let month = ("0" + (inDate.getMonth() + 1)).slice(-2);
+    let year = inDate.getFullYear();
+    let date = [day, month, year].join('-');
+    return date;
+}
 
 function updateTimer(){
     voiceChannels.forEach(vChannel => {
         vChannel.members.forEach(member => {
             if(!users.has(member.id) && !member.user.bot){
-                users.set(member.id, {user: member, workTime: 0, isOnServer: true});
-                console.log(`${member.user.username} joined for the first time`)
+                users.set(member.id, {user: member, workTime: 0, isOnServer: true, joinTime: today.getHours() + ":" + today.getMinutes()});
+                console.log(`${member.user.username} joined for the first time today`)
             } else if(users.has(member.id) && !users.get(member.id).isOnServer) {
-                console.log(`${member.user.username} returned to the server :)`);
+                console.log(`${member.user.username} returned to the server`);
                 users.get(member.id).isOnServer = true;
             }
         });
@@ -32,15 +41,14 @@ function updateTimer(){
         });
 
         if(hasLeft){
-            let today = new Date();
-            let day = today.getDate()
-            let month = today.getMonth() + 1;
-            let year = today.getFullYear();
-            let date = [day, month, year].join('-');
+            let date = getStringDate(today)
+
+            let hours = Math.floor(member.workTime / 60);
+            let minutes = member.workTime - hours * 60;
 
             // If left, send him today's work time
-            console.log(`${member.user.displayName} left the server :(`);
-            member.user.send(`[${date}] Work time: ${member.workTime} min`);
+            console.log(`${member.user.displayName} left the server`);
+            member.user.send(`You have started working today at: ${member.joinTime}\n[${date}] Work time: ${hours} h ${minutes} min`);
             member.isOnServer = false;
         } else{
             // Increment work time
@@ -64,12 +72,17 @@ bot.on('ready', () => {
         }
     });
 
+    console.log(`Bot started on ${getStringDate(today)}`)
+
     // Initial user count
     updateTimer();
 });
 
 // Handle messages
 bot.on('message', msg => {
+    if(msg.channel.name === 'timer-bot'){
+        
+    }
     if (msg.content === 'users'){
         msg.delete();
         voiceChannels.forEach(vChannel => {
@@ -83,7 +96,25 @@ bot.on('message', msg => {
 // Check if user left or new joined
 bot.setInterval(() => {
     if(!server) return;
+
+    if(getStringDate(today) !== getStringDate(new Date())){
+        today = new Date();
+        console.log("NEW DAY!")
+
+        users.each(member => {
+            let date = getStringDate(today)
+
+            let hours = Math.floor(member.workTime / 60);
+            let minutes = member.workTime - hours * 60;
+
+            member.user.send(`Day has ended! GJ!\nYou have started working today at: ${member.joinTime}\n[${date}] Work time: ${hours} h ${minutes} min`);
+        });
+
+        users.clear();
+    }
+
     updateTimer();
-}, 10000)
+    console.log("Updating timers!");
+}, 60000)
 
 bot.login(process.env.BOT_SECRET);
